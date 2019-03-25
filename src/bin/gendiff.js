c79ccd397c7f2034335a46bcdd8ce4c02904ad1e
Amidery <zhenev.eva.me@gmail.com>
2019-03-25 12:48:10 +0700

    updated version and project keywords

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,7 @@
 
 import programm from 'commander';
 
-programm.version('0.0.1')
+programm.version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig>, <secondConfig>')
   .option('-f, --format [type]', 'Output format')
