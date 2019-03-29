9f31a6ad6cdd26aa605059726cdc401ef4d3aec3
Amidery <zhenev.eva.me@gmail.com>
2019-03-30 00:27:14 +0700

    add nested files compare logic

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,19 +5,37 @@ const files = [
   {
     filepath1: '__tests__/__fixtures__/before.json',
     filepath2: '__tests__/__fixtures__/after.json',
+    pathtoresult: '__tests__/__fixtures__/result.txt',
   },
   {
     filepath1: '__tests__/__fixtures__/before.yml',
     filepath2: '__tests__/__fixtures__/after.yml',
+    pathtoresult: '__tests__/__fixtures__/result.txt',
   },
   {
     filepath1: '__tests__/__fixtures__/before.ini',
     filepath2: '__tests__/__fixtures__/after.ini',
+    pathtoresult: '__tests__/__fixtures__/result.txt',
+  },
+  {
+    filepath1: '__tests__/__fixtures__/beforeComplex.json',
+    filepath2: '__tests__/__fixtures__/afterComplex.json',
+    pathtoresult: '__tests__/__fixtures__/resultComplex.txt',
+  },
+  {
+    filepath1: '__tests__/__fixtures__/beforeComplex.yml',
+    filepath2: '__tests__/__fixtures__/afterComplex.yml',
+    pathtoresult: '__tests__/__fixtures__/resultComplex.txt',
+  },
+  {
+    filepath1: '__tests__/__fixtures__/beforeComplex.ini',
+    filepath2: '__tests__/__fixtures__/afterComplex.ini',
+    pathtoresult: '__tests__/__fixtures__/resultComplex.txt',
   },
 ];
 
-test.each(files)('compare files', ({ filepath1, filepath2 }) => {
-  const expectedResult = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8');
+test.each(files)('compare files', ({ filepath1, filepath2, pathtoresult }) => {
+  const expectedResult = fs.readFileSync(pathtoresult, 'utf-8');
   const actualResult = gendiff(filepath1, filepath2);
   expect(actualResult).toBe(expectedResult);
 });
--- src/index.js
+++ src/index.js
@@ -3,35 +3,82 @@ import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 
+const getAST = (data1, data2) => {
+  const dataBeforeKeys = Object.keys(data1);
+  const dataAfterKeys = Object.keys(data2);
+
+  const uniqueDataKeys = _.flattenDeep(dataAfterKeys.reduce(
+    (acc, key) => (_.has(data1, key) ? acc : [acc, key]), dataBeforeKeys,
+  ));
+
+  const root = {
+    keyname: '',
+    value: '',
+    status: '',
+    children: [],
+  };
+
+  const getDiff = (acc, key) => {
+    if (_.has(data1, key) === _.has(data2, key)) {
+      if (data1[key] instanceof Object && data2[key] instanceof Object) {
+        return { ...acc, keyname: key, children: getAST(data1[key], data2[key]) };
+      }
+      if (data1[key] === data2[key]) {
+        return { ...acc, keyname: key, value: data1[key] };
+      }
+      return [{
+        ...acc, keyname: key, status: 'added', value: data2[key],
+      }, {
+        ...acc, keyname: key, status: 'removed', value: data1[key],
+      }];
+    }
+    if (!_.has(data2, key)) {
+      return {
+        ...acc, keyname: key, status: 'removed', value: data1[key],
+      };
+    }
+    return {
+      ...acc, keyname: key, status: 'added', value: data2[key],
+    };
+  };
+
+  const ast = _.flattenDeep(uniqueDataKeys.reduce((acc, key) => [...acc, getDiff(root, key)], []));
+  return ast;
+};
+
+const getStatus = (status) => {
+  switch (status) {
+    case 'added':
+      return '+ ';
+    case 'removed':
+      return '- ';
+    default:
+      return '  ';
+  }
+};
+
+const getSpaces = spacesAmount => ' '.repeat(spacesAmount);
+
+const getValue = (value, spacesAmount) => {
+  if (value instanceof Object) {
+    return ['{\n', Object.keys(value).map(key => `${getSpaces(spacesAmount + 6)}${key}: ${value[key]}`), `\n${getSpaces(spacesAmount + 2)}}`].join('');
+  }
+  return value;
+};
+
+const render = (ast, spacesAmount = 2) => {
+  const result = ast.map(obj => `${getSpaces(spacesAmount)}${getStatus(obj.status)}${obj.keyname}: ${getValue(obj.value, spacesAmount)}${obj.children.length > 0 ? render(obj.children, spacesAmount + 4) : ''}`);
+  return ['{', ...result, `${getSpaces(spacesAmount - 2)}}`].join('\n');
+};
+
 const getContent = filepath => fs.readFileSync(filepath, 'utf-8');
 const getExt = filepath => path.extname(filepath);
 
 const gendiff = (pathToFile1, pathToFile2) => {
   const dataBefore = parse(getContent(pathToFile1), getExt(pathToFile1));
   const dataAfter = parse(getContent(pathToFile2), getExt(pathToFile2));
-
-  const dataBeforeKeys = Object.keys(dataBefore);
-  const dataAfterKeys = Object.keys(dataAfter);
-
-  const uniqueDataKeys = _.flattenDeep(dataAfterKeys.reduce(
-    (acc, key) => (_.has(dataBefore, key) ? acc : [acc, key]), dataBeforeKeys,
-  ));
-
-  const getDiff = (acc, key) => {
-    if (_.has(dataBefore, key) === _.has(dataAfter, key)) {
-      if (dataBefore[key] === dataAfter[key]) {
-        return [...acc, `    ${key}: ${dataBefore[key]}`];
-      }
-      return [...acc, `  + ${key}: ${dataAfter[key]}`, `  - ${key}: ${dataBefore[key]}`];
-    }
-    if (!_.has(dataAfter, key)) {
-      return [...acc, `  - ${key}: ${dataBefore[key]}`];
-    }
-    return [...acc, `  + ${key}: ${dataAfter[key]}`];
-  };
-
-  const result = uniqueDataKeys.reduce((acc, key) => getDiff(acc, key), '');
-  return ['{', ...result, '}\n'].join('\n');
+  const ast = getAST(dataBefore, dataAfter);
+  return render(ast);
 };
 
 export default gendiff;
