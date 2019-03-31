20201f8aac4dc70334da922bd6b0e6e8a5188832
Amidery <zhenev.eva.me@gmail.com>
2019-03-31 13:18:21 +0700

    update file

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,8 +6,8 @@ import gendiff from '..';
 programm.version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .action((firstConfig, secondConfig, outputformat) => {
-    console.log(gendiff(firstConfig, secondConfig, outputformat));
+  .action((firstConfig, secondConfig) => {
+    console.log(gendiff(firstConfig, secondConfig, programm.format));
   })
   .option('-f, --format [type]', 'Output format', 'pretty')
   .parse(process.argv);
