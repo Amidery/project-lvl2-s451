b6d023841036211394fc8b405747573731f131ea
Amidery <zhenev.eva.me@gmail.com>
2019-03-31 14:01:21 +0700

    add json output format

--- src/renderers.js
+++ src/renderers.js
@@ -4,6 +4,7 @@ import renderPlain from './renderers/plainformat';
 const outputFormats = {
   pretty: renderPretty,
   plain: renderPlain,
+  json: JSON.stringify,
 };
 
 const render = (diffContent, format) => outputFormats[format](diffContent);
